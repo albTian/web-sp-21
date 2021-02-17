856e702300c1fb0eb94c0ee80999fd24fda588f9
Albert Tian <atian@berkeley.edu>
2021-02-16 23:32:43 -0500

    fruit check in place

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
 
+function FruitInfo(props) {
+    return (
+        <>
+            <h3>{props.name}</h3>
+            <div>{props.fruitName}</div>
+            <img src={props.fruitPic} width="200px" alt="put your fruit picture in /static/img/fruits"/>
+        </>
+    )
+}
+
 export default function AboutPage() {
     return (
         <>
-            <div>About page lmao</div>
-            <Link to="/">back home</Link>
+            <div>About page lmao, add ur fav fruits below by editing the code</div>
+            <Link to="/">Back home</Link>
+            {/* Add your fruits below! */}
+            <FruitInfo name="Albert" fruitName="i love me some peaches!" fruitPic="/img/fruits/peaches.jpg"/>
         </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
+
+
 export default function Home() {
   return(
     <>
-      <div>albert ahah Hello world!</div>
+      <div>Hi web team! If you got here then nice :) </div>
+      <h3>add your name and favorite fruit to the about page</h3>
       <Link to="/about">to about page</Link>
     </>
   ) 
