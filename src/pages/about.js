372c9351ee0f9fc8618077d4ef015748b37e30f9
Albert Tian <atian@berkeley.edu>
2021-02-16 20:41:59 -0500

    Added linking

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function AboutPage() {
+    return (
+        <>
+            <div>About page lmao</div>
+            <Link to="/">back home</Link>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+    <>
+      <div>albert ahah Hello world!</div>
+      <Link to="/about">to about page</Link>
+    </>
+  ) 
 }
